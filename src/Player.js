e550bda7669f9e516353839e1a3dfafb1496f0b3
Othmane Merzak <otischamp@gmail.com>
2024-03-19 12:38:54 +0000

    Last Commit

--- src/Player.js
+++ src/Player.js
@@ -1,20 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 
-export default function Player (props) {
+export default function Player ({playerName, playerTeam, playerNation, playerNumber, playerAge, playerImg}) {
     return (
         
         
         <Card className='bg-danger' style={{ width: '18rem' }}>
         <Card.Body className='bg-success'>
-          <Card.Title>{props.name}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Team: {props.team}</Card.Subtitle>
+          <Card.Title>{playerName}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Team: {playerTeam}</Card.Subtitle>
           <Card.Text>
-          Nationality: {props.nationality}<br/>
-          Shirt Number: {props.shirtNumber}<br/>
-          Age: {props.age}
+          Nationality: {playerNation}<br/>
+          Shirt Number: {playerNumber}<br/>
+          Age: {playerAge}
           </Card.Text>
-          <img src={props.imgUrl} style={{width:"100%"}}></img>
+          <img src={playerImg} style={{width:"100%"}}></img>
           </Card.Body>
       </Card>
         
